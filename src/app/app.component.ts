ea5b973947332c59d257c83bc47670ea0c17b6a5
FahmiDeveloper <coursangularfahmi@gmail.com>
2020-12-14 12:17:02 +0100

    check box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularMaterial';
+
+  isChecked = true;
+
+  onChange($event){
+    console.log($event)
+  }
 }
