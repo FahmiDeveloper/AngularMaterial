9b70ec2308ca328c70a4eed8a6c0366f4d008ee5
FahmiDeveloper <coursangularfahmi@gmail.com>
2020-12-20 11:17:45 +0100

    chips

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularMaterial';
+  
+  categories = [
+    { name: 'Beginner' },
+    { name: 'Intermediate' },
+    { name: 'Advanced' }
+  ];
+
+  onSelectCategory(category){
+    this.categories
+    .filter(c => c != category)
+    .forEach(c => c['selected'] = false);
+
+    category.selected = !category.selected;
+  }
 }
